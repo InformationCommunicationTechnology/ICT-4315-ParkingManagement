b75d1fea7c693ac36b59a7ad8885c3df4a449c37
Luther <luther.chikumba@guildeducation.com>
2023-01-03 12:59:02 -0700

    ICT 4305 Solution File

---
+++ ParkingManagement/src/test/java/edu/du/ict4315/parkingmanagement/ParkingLotTypeTest.java
@@ -0,0 +1,20 @@
+package edu.du.ict4315.parkingmanagement; /**
+ * @Course: ICT 4315
+ * @Project: ParkingManagement
+ * @Instructor: Mike Prasad
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @Date: 1/3/23
+ * @author lutherchikumba
+ *
+ */
+public class ParkingLotTypeTest {
+
+      public ParkingLotTypeTest() {
+
+      }
+}
